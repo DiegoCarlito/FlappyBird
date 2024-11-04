62d33a12cbcae6cf678b87a74070babf3257b15f
DiegoCarlito <diego.carlito01@gmail.com>
2024-11-03 21:18:22 -0300

    add character movement

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true) {
+            myRigidbody.velocity = UnityEngine.Vector2.up * 10;
+        }
     }
 }
